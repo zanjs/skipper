d015be8fd1a7d81ee02813f45884e1ce7fbaeabe
zanjs <root@zanjs.com>
2016-12-02 18:09:50 +0800

    menu update :full_moon:

--- resources/views/menus/browse.blade.php
+++ resources/views/menus/browse.blade.php
@@ -4,7 +4,7 @@
     <h1 class="page-title">
         <i class="skipper-list-add"></i> {{ $dataType->display_name_plural }}
         <a href="{{ route($dataType->slug.'.create') }}" class="btn btn-success">
-            <i class="skipper-plus"></i> Add New
+            <i class="skipper-plus"></i> 添加新的
         </a>
     </h1>
 @stop
@@ -16,8 +16,8 @@
 @section('content')
     <div class="container-fluid">
         <div class="alert alert-info">
-            <strong>How To Use:</strong>
-            <p>You can output a menu anywhere on your site by calling <code>Menu::display('name')</code></p>
+            <strong>如何使用:</strong>
+            <p>您可以在网站上任意位置 通过调用 <code>Menu::display('name')</code> 获取的每个设置的值 </p>
         </div>
     </div>
 
@@ -49,13 +49,13 @@
                                     @endforeach
                                     <td class="no-sort no-click">
                                         <div class="btn-sm btn-danger pull-right delete" data-id="{{ $data->id }}">
-                                            <i class="skipper-trash"></i> Delete
+                                            <i class="skipper-trash"></i> 删除
                                         </div>
                                         <a href="{{ route('menus.edit', $data->id) }}" class="btn-sm btn-primary pull-right edit">
-                                            <i class="skipper-edit"></i> Edit
+                                            <i class="skipper-edit"></i> 编辑
                                         </a>
                                         <a href="{{ route('skipper.menu.builder', $data->id) }}" class="btn-sm btn-success pull-right">
-                                            <i class="skipper-list"></i> Builder
+                                            <i class="skipper-list"></i> 管理
                                         </a>
                                     </td>
                                 </tr>
@@ -76,16 +76,16 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                     <h4 class="modal-title">
-                        <i class="skipper-trash"></i> Are you sure you want to delete this {{ $dataType->display_name_singular }}?
+                        <i class="skipper-trash"></i> 你确定要删除这个 {{ $dataType->display_name_singular }}?
                     </h4>
                 </div>
                 <div class="modal-footer">
                     <form action="{{ route('menus.index') }}" id="delete_form" method="POST">
                         <input type="hidden" name="_method" value="DELETE">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                        <input type="submit" class="btn btn-danger pull-right delete-confirm" value="Yes, Delete This {{ $dataType->display_name_singular }}">
+                        <input type="submit" class="btn btn-danger pull-right delete-confirm" value="是的删除 {{ $dataType->display_name_singular }}">
                     </form>
-                    <button type="button" class="btn btn-default pull-right" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-default pull-right" data-dismiss="modal">取消</button>
                 </div>
             </div>
         </div>
